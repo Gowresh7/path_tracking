50308be064fc1822bc5748f8a065fe3a92e86436
Gowresh7 <gowresh7@gmail.com>
2024-08-27 21:50:27 +0400

    docker run scripts

---
+++ docker_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the DISPLAY environment variable
+export DISPLAY=${DISPLAY}
+
+# Run the Gazebo container with ROS and additional commands
+docker run -it \
+  --env="DISPLAY=${DISPLAY}" \
+  --gpus all \
+  --env="QT_X11_NO_MITSHM=1" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --device=/dev/dri:/dev/dri \
+  --name gazebo_container \
+  gazebo \
+  bash -c "source /opt/ros/noetic/setup.bash && \
+           source ~/gem_ws/devel/setup.bash && \
+           roslaunch path_tracking path_tracking_sim.launch & \
+           sleep 20 && \
+           rosrun path_tracking path_publisher.py"
